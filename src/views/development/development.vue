7f08d49237930aa7dc44425a8fdd51f56eec42a9
caogit <1143216961@qq.com>
2020-12-15 16:14:28 +0800

    3

---
+++ src/views/development/development.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="development">
+      <div id="development_header">
+          <ul class="development_header_l">
+              <li><img src="@/assets/33e5b2cd9ba14ca0879daf7b036ed70c.png" alt=""></li>
+          </ul>
+          <ul class="development_header_r">
+
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+name:'development',
+}
+</script>
+
+<style lang='scss'>
+#development{
+    width: 100vw;
+    height: 100vh;
+    #development_header{
+        width: 100%;
+        height: 62px;
+        background-color: #2d323d;
+        #development_header_l{
+            li{
+                &:nth-child(1){
+                    &>img{
+                        width: 172px;
+                        height: 30px;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
